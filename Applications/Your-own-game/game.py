40b49735e2beb7a76694a5a67a618c31393547a3
abdullahmahmood <abdullah.au@outlook.com>
2024-05-21 12:40:48 +0400

    update

---
+++ Applications/Your-own-game/game.py
@@ -0,0 +1,7 @@
+# The game has a sprite the player can move in some way
+# The game has some collectable items and/or enemies
+# The player needs to be set a clear task in the game
+# The game contains a counter which tells the player how they are doing in the game
+# The source code for the game is divided into functions like in the Sokoban example
+
+
